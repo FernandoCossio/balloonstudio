5fd7a353fba04bf99183856c4859a43f14ee7963
FernandoCossio <fcossio0x41@gmail.com>
2026-05-18 21:49:26 -0400

    feat[BACK] Crear decorador de currentUserId

---
+++ backend/ms-decoraciones/src/main/java/com/decoraciones/common/decorators/CurrentUserIdResolver.java
@@ -0,0 +1,47 @@
+package com.decoraciones.common.decorators;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@Slf4j
+public class CurrentUserIdResolver implements HandlerMethodArgumentResolver {
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.hasParameterAnnotation(CurrentUserId.class) && Long.class.equals(parameter.getParameterType());
+	}
+
+	@Override
+	public Long resolveArgument(
+		MethodParameter parameter,
+		ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest,
+		WebDataBinderFactory binderFactory
+	) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication == null || !authentication.isAuthenticated()) {
+			return null;
+		}
+
+		Object principal = authentication.getPrincipal();
+		if (principal instanceof Jwt jwt) {
+			Long uid = jwt.getClaimAsLong("uid");
+			if (uid == null) {
+				log.warn("JWT sin claim 'uid' en @CurrentUserId");
+			}
+			return uid;
+		}
+
+		return null;
+	}
+}
