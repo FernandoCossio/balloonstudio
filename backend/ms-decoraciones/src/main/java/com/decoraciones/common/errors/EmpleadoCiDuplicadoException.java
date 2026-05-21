ad2235c77a9e85fd1d4e1abca7aab0acfddb0917
AxelMQ <axelmamaniquispia@gmail.com>
2026-05-21 17:39:10 -0400

    refactor [BACK] Eliminar modelo de empleado

--- backend/ms-decoraciones/src/main/java/com/decoraciones/common/errors/EmpleadoCiDuplicadoException.java
+++
@@ -1,7 +0,0 @@
-package com.decoraciones.common.errors;
-
-public class EmpleadoCiDuplicadoException extends AppException {
-	public EmpleadoCiDuplicadoException() {
-		super(ErrorCode.EMPLEADO_CI_DUPLICADO);
-	}
-}
